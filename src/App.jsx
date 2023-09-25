0304a93e486f5c3100de0c560049e44b7d04cb68
miistrass <milenastrass@gmail.com>
2023-09-25 19:59:39 -0300

    adicionando map e filter para completar e remover

--- src/App.jsx
+++ src/App.jsx
@@ -27,15 +27,42 @@ function App() {
     }
   ])
 
+  const addTodo = (text, category) => {
+
+    const newTodo = [...todos,
+      {
+        id: Math.floor(Math.random() * 1000),
+        text,
+        category,
+        isCompleted: false,
+      }
+    ]
+
+    setTodos(newTodo)
+
+  }
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null)
+    setTodos(filteredTodos)
+  }
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
+    setTodos(newTodos)
+  }
+
   return (
     <div className='app'>
       <h1>Lista de tarefas</h1>
       <div className='todo-list'>
         {todos.map((todo) => (
-          <Todo todo={todo}/>
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
         ))}
       </div>
-      <TodoForm />
+      <TodoForm addTodo={addTodo}/>
     </div>
   )
 }
